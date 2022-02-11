c4bc48b6d11dd4c9c61e98a7fe606a441aac5a45
RivkaShelli <93613379+RivkaShelli@users.noreply.github.com>
2022-02-11 12:12:04 +0200

    Create Product.ts

---
+++ Product.ts
@@ -0,0 +1,13 @@
+class Product { 
+    price: number;
+    productName: string;
+    company: string;
+    code: string;
+    constructor(price: number, productName: string, company: string, code: string) {
+        this.price = price;
+        this.productName = productName;
+        this.company = company;
+        this.code = code;
+
+    }
+}
